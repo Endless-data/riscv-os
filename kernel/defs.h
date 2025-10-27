527a009480123895eef4b9d3f70e2f6906286bfb
ZhenTong <910898041@qq.com>
2025-10-27 14:47:56 +0800

    Experiment 3: Page Tables and Memory Management
    Through in-depth analysis of the xv6 memory management system, understand the workings of virtual memory and independently implement a physical memory allocator and page table management system.

--- kernel/defs.h
+++ kernel/defs.h
@@ -21,4 +21,24 @@ int sprintf(char *buf, const char *fmt, ...);
 int printf_color(int color, const char *fmt, ...);
 void printf_init(void);
 
+// kalloc.c - 物理内存管理
+void pmm_init(void);
+void* alloc_page(void);
+void free_page(void* page);
+void* alloc_pages(int n);
+uint64 get_free_pages(void);
+void print_memory_stats(void);
+
+// vm.c - 虚拟内存管理
+pagetable_t create_pagetable(void);
+void destroy_pagetable(pagetable_t pt);
+pte_t* walk_create(pagetable_t pt, uint64 va);
+pte_t* walk_lookup(pagetable_t pt, uint64 va);
+int map_page(pagetable_t pt, uint64 va, uint64 pa, int perm);
+int map_range(pagetable_t pt, uint64 va, uint64 size, uint64 pa, int perm);
+void dump_pagetable(pagetable_t pt, int level);
+uint64 va2pa(pagetable_t pt, uint64 va);
+void kvminit(void);
+void kvminithart(void);
+
 #endif // _DEFS_H_
